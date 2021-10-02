9ad0a3820758ed30832ab1b9fdce68e97c63d57b
Rafael Soares Gabriel <88918826+rafaelsoares07@users.noreply.github.com>
2021-10-02 15:46:29 -0300

    Add files via upload

---
+++ listaDeMusicas.js
@@ -0,0 +1,13 @@
+let listaMusicas = [
+	
+	{titulo:'Sombra e Dúvida', banda: ' Boogarins', src: 'musicas/boogarins.mp3', img:'imagens/boogarins.jpg'},
+
+	{titulo:'GAME', banda: 'Duda Beat', src: 'musicas/dudaBeat.mp3', img:'imagens/dudaBeat.jpg'},
+
+	{titulo:'Me Toca', banda: ' Mariana Sena', src: 'musicas/marianaSena.mp3', img:'imagens/marinaSena.jpg'},
+
+	{titulo:'Maya', banda: ' Tagore', src: 'musicas/tagore.mp3', img:'imagens/tagore.jpg'}
+
+
+
+];
